b7922fe13ef9a13f91e3744b25c9d15735e6a17f
impranaav <129495632+impranaav@users.noreply.github.com>
2024-05-02 19:06:18 +0530

    bug fix
    handled negative scenarios

--- factorial.py
+++ factorial.py
@@ -1,11 +1,9 @@
 #  Write a program to find the factorial of a given number n.
- number = int(input("Choose a number?"))
-product=1
-for i in range(1,number+1):
-    product= product * i
-print(product)
-
-
-
-
-
+number = int(input("Choose a number? "))
+if number < 0:
+    print("Factorial not defined for negative numbers")
+else:
+    product=1
+    for i in range(1,number+1):
+        product= product * i
+    print(f"factorial of {number} is {product}" )

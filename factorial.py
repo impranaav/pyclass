8f458b4d65ee0e8011a5d7ac5abb94182775eb66
impranaav <129495632+impranaav@users.noreply.github.com>
2024-05-02 18:41:31 +0530

    Add files via upload
    Factorial  
    Question-  Write a program to find the factorial of a given number n.

---
+++ factorial.py
@@ -0,0 +1,11 @@
+#  Write a program to find the factorial of a given number n.
+ number = int(input("Choose a number?"))
+product=1
+for i in range(1,number+1):
+    product= product * i
+print(product)
+
+
+
+
+
